3ef07a9b5c7d4eeb8dfe7f1f3c8d3eb393523757
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2021-07-12 17:33:05 -0300

    Exercicio 68 - rodando

--- exerc068.py
+++ exerc068.py
@@ -2,4 +2,33 @@
 # O jogo só será interrompido quando o jogador perder,
 # mostrando o total de vitórias consecutivas que ele conquistou
 # no final do jogo.
+import random
 
+print('=' * 20)
+print('Jogo do PAR ou ÍMPAR')
+print('=' * 20)
+
+while True:
+    num = int(input('Escolha um número: '))
+    tipo = str(input('Par ou Ímpar [P/I]? ')).upper().strip()
+    if tipo == 'P':
+        tipo = 'PAR'
+    else:
+        tipo = 'ÍMPAR'
+    computador = random.randint(1, 10)
+    resultado = num + computador
+    if resultado % 2 == 0:
+        res = 'PAR'
+    else:
+        res = 'ÍMPAR'
+
+    print(f'Você escolheu [{num}]\nComputador escolheu [{computador}]\n Você pediu {tipo}')
+
+    print('~' * 18)
+    print(f'Resultado foi [ {res} ]!')
+    if tipo == res:
+        print('Você VENCEU!')
+    else:
+        print('Computador VENCEU!')
+        break
+    print('~' * 18)
