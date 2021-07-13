a0a128d6a5a7a99627509d05cad21ebc60e06307
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2021-07-12 23:02:04 -0300

    Mundo 3 - Exercicio 69, 72, 73 (68 deletado)

---
+++ exerc072.py
@@ -0,0 +1,14 @@
+#Crie um programa que tenha uma dupla totalmente preenchida com uma
+# contagem por extenso, de zero até vinte. Seu programa deverá ler
+# um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+numExtenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis',\
+             'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', \
+             'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', \
+             'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número de 0 a 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente.', end='')
+print(f'Você digitou [{numExtenso[ num ]}]')
