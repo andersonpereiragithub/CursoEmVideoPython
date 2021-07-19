22eb8b02092cd09c896af2002da5ed70ff33d802
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2021-07-19 13:04:54 -0300

    Exercicio 78 - COMPLETO

--- exerc078.py
+++ exerc078.py
@@ -4,12 +4,22 @@
 #as sua respectivas posições na lista.
 valores = []
 for v in range(0, 5):
-    valores.append(int(input(f'Digiver o {v + 1}º Valor: ')))
+    valores.append(int(input(f'Digiver um valor para posicão {v}: ')))
     if v == 0:
-        menor = maior = v
-    if v < menor:
-        menor = v
-    if v > maior:
-        maior = v
-print(f'Maior [ {maior} ]')
-print(f'Menor [ {menor} ]')
+        menor = maior = valores[v]
+    else:
+        if valores[v] < menor:
+            menor = valores[v]
+        if valores[v] > maior:
+            maior = valores[v]
+print('-=' * 30)
+print(f'Você digitou os valores {valores} ')
+print(f'Maior valor é: [{maior}] nas posições', end='')
+for pos, v in enumerate(valores):
+    if v == maior:
+        print(f' [{pos}]...', end='')
+print()
+print(f'Menor valor é: [ {menor} ] nas posições', end='')
+for pos, v in enumerate(valores):
+    if v == menor:
+        print(f' [{pos}]...', end='')
