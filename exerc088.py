0ec22ad50b7494b51daaada7cd1468e47d679e79
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2021-08-04 23:38:35 -0300

    Exercicio 88 - inicio

--- exerc088.py
+++ exerc088.py
@@ -2,3 +2,12 @@
 # palpites.O programa vai perguntar quantos jogos serão
 # gerados e vai sortear 6 números entre 1 e 60 para cada jogo,
 # cadastrando tudo em uma lista composta.
+from random import randint
+
+print('-' * 30)
+print(f'      JOGA NA MEGA SENA     ')
+print('-' * 30)
+
+qtdNum = int(input('Quantos jogos serão gerados? '))
+lista = []
+num = randint(1, 60)
