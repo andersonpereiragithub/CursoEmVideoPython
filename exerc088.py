b990a92a9ad8a1068b88cd2fa57239b3f65b9859
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2021-08-05 11:29:24 -0300

    Exercicio 88 completo - Fiferente da solucao do Professor

--- exerc088.py
+++ exerc088.py
@@ -3,11 +3,24 @@
 # gerados e vai sortear 6 números entre 1 e 60 para cada jogo,
 # cadastrando tudo em uma lista composta.
 from random import randint
-
+from  time import sleep
 print('-' * 30)
 print(f'      JOGA NA MEGA SENA     ')
 print('-' * 30)
-
-qtdNum = int(input('Quantos jogos serão gerados? '))
 lista = []
-num = randint(1, 60)
+listaFinal = []
+qtdeNum = 1 + int(input('Quantos jogos serão gerados? '))
+tot = 0
+for n in range(1, qtdeNum):
+    while tot < 6:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            tot += 1
+    lista.sort()
+    listaFinal.append(lista[:])
+    lista.clear()
+    tot = 0
+for pos, l in enumerate(listaFinal):
+    print(f'Jogo nº {pos + 1}: {l}')
+    sleep(1)
