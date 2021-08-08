37b98bffeb5391110015848d8d9f9636bcaebfa1
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2021-08-08 10:57:45 -0300

    Exercicio 94 FEITO

--- exerc094.py
+++ exerc094.py
@@ -5,18 +5,41 @@
 # B) A média de idade
 # C) Uma lista com as mulheres
 # D) Uma lista de pessoas com idade acima da média
-pessoa = []
+
 dict_pessoas = dict()
 lista_dados = []
+soma = media = 0
 while True:
-    pessoa.append(str(input('Nome: ')))
-    pessoa.append(str(input('Sexo [M/F]: ')))
-    pessoa.append(int(input('Idade: ')))
-    dict_pessoas = pessoa[:]
-    pessoa.clear()
-    lista_dados.append(dict_pessoas)
-    resp = str(input('Deseja cadastrar outra? [S/N]'))
-    if resp in 'Nn':
+    dict_pessoas['nome'] = str(input('Nome: '))
+    while True:
+        dict_pessoas['sexo'] = str(input('Sexo [M/F]: ')).upper()[0]
+        if dict_pessoas['sexo'] in 'MF':
+            break
+        print('ERRO! Digite apenas M ou F.')
+    dict_pessoas['idade'] = int(input('Idade: '))
+    soma += dict_pessoas['idade']
+    lista_dados.append(dict_pessoas.copy())
+    while True:
+        resp = str(input('Deseja cadastrar outra? [S/N]')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp  == 'N':
         break
-print(f'Foram cadastradas {len(lista_dados)} pessoas.')
+print('-=' * 20)
+print(f'A) Foram cadastradas {len(lista_dados)} pessoas.')
+media = soma / len(lista_dados)
+print(f'B) A média de idade é de [{media:5.2f}] anos.')
+print(f'C) As mulheres cadastradas foram ', end = '')
+for p in lista_dados:
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]}', end = ' ')
 print()
+print(f'As pessoas com idade acima da média são: ')
+for p in lista_dados:
+    if p['idade'] >= media:
+        print(f'     ', end = ' ')
+        for k, v in p.items():
+            print(f'{k} = {v} ', end='')
+        print()
+print('<<< ENCERRADO >>>')
